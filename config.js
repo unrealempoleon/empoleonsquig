bb91a3f392dd9033e40fdea8e45e14abee88bf52
unrealempoleon <nocontextempoleon@gmail.com>
2026-09-20 23:25:42 +0800

    Update config.js

--- config.js
+++ config.js
@@ -43,7 +43,7 @@ const init_phones = ["BKF"],            // Optional. Which graphs to display on 
 
 // Specify which targets to display
 const targets = [
-    { type:"Neutral",    files:["Diffuse Field","Etymotic","Free Field","Innerfidelity ID"] },
+    { type:"Neutral",    files:["Diffuse Field","Etymotic","IEF Neutral 2020","IEF Neutral 2023"] },
     { type:"Reviewer",   files:["Antdroid","Bad Guy","Banbeucmas","Crinacle","Precogvision","Super Review"] },
     { type:"Preference", files:["Harman","Rtings","Sonarworks"] }
 ];
